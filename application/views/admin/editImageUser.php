dc83d7dc6c102b7734fc5de08532fcb7fdc198ee
winicofazry <nikofazry@gmail.com>
2021-06-05 21:17:19 +0700

    viewadmin, raw, css update

---
+++ application/views/admin/editImageUser.php
@@ -0,0 +1,64 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Edit Foto Barang</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                        <li class="breadcrumb-item active">Edit Foto Barang</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-10 mx-auto">
+                                <div class="img-profile">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <?php echo form_open_multipart('admin/editImageProfile/' . $admin['id_user']); ?>
+                                            <input type="file" name="image" class="mb-4" />
+                                            <br />
+                                            <div class="col-md-12">
+                                                <div class="d-flex justify-content-center">
+                                                    <input type="submit" value="Upload" class="btn btn-primary" />
+                                                </div>
+                                            </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+    </div>
+</aside>
+<!-- /.control-sidebar -->
